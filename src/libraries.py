bcb7340ede24f3ca155522ccf12fba7390ce614b
Muhammad Sarim Asrar <masrar000@citymail.cuny.edu>
2025-04-26 01:34:42 -0400

    centralizes shared imports and async setups

---
+++ src/libraries.py
@@ -0,0 +1,22 @@
+# libraries.py
+import os, re, glob
+from collections import defaultdict
+
+import pandas as pd
+from openpyxl import Workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
+
+
+import streamlit as st
+import nest_asyncio
+
+from dotenv import load_dotenv, find_dotenv
+
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_community.vectorstores import LanceDB
+
+# ensure async compatibility
+nest_asyncio.apply()
